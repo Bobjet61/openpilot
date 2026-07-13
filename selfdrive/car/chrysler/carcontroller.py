8f7a0e3385581fe2d1835adf26bcd5c602507549
Bobjet61 <53230707+Bobjet61@users.noreply.github.com>
2026-07-12 21:03:17 -0700

    Update carcontroller.py

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -8,31 +8,30 @@ from openpilot.selfdrive.car.chrysler import chryslercan
 from openpilot.selfdrive.car.chrysler.values import RAM_CARS, RAM_DT, CarControllerParams, ChryslerFlags, ChryslerFlagsSP
 from openpilot.selfdrive.car.interfaces import CarControllerBase, FORWARD_GEARS
 from openpilot.selfdrive.controls.lib.drive_helpers import FCA_V_CRUISE_MIN
- 
+
 BUTTONS_STATES = ["accelCruise", "decelCruise", "cancel", "resumeCruise"]
- 
+
 # Chrysler/Jeep standstill brake-hold (stop-and-go): re-tap "resume" before the Jeep's
 # native ~2s ACC release, so the brake stays held in traffic and it follows again when the
 # lead moves. Uses only the existing cruise-button path -- no messaging changes.
-# Disable at runtime: params.put_bool("ChryslerStandstillHoldDisabled", True)
 STANDSTILL_HOLD_TAP_INTERVAL = 75   # frames (~0.75s) between resume taps; must beat the ~2s release
 STANDSTILL_HOLD_TAP_LEN = 10        # frames (~0.10s) each tap is held
- 
- 
+
+
 class CarController(CarControllerBase):
   def __init__(self, dbc_name, CP, VM):
     self.CP = CP
     self.apply_steer_last = 0
     self.frame = 0
- 
+
     self.hud_count = 0
     self.last_lkas_falling_edge = 0
     self.lkas_control_bit_prev = False
     self.last_button_frame = 0
- 
+
     self.packer = CANPacker(dbc_name)
     self.params = CarControllerParams(CP)
- 
+
     self.sm = messaging.SubMaster(['longitudinalPlanSP'])
     self.param_s = Params()
     self.is_metric = self.param_s.get_bool("IsMetric")
@@ -63,14 +62,13 @@ class CarController(CarControllerBase):
     self.m_tsc = 0
     self.steady_speed = 0
     self.button_frame = 0
-    self.standstill_hold_enabled = not self.param_s.get_bool("ChryslerStandstillHoldDisabled")
     self.last_standstill_tap = -STANDSTILL_HOLD_TAP_INTERVAL
     self.standstill_tap_end = 0
- 
+
   def update(self, CC, CS, now_nanos):
     if not self.CP.pcmCruiseSpeed:
       self.sm.update(0)
- 
+
       if self.sm.updated['longitudinalPlanSP']:
         self.v_tsc_state = self.sm['longitudinalPlanSP'].visionTurnControllerState
         self.slc_state = self.sm['longitudinalPlanSP'].speedLimitControlState
@@ -79,39 +77,38 @@ class CarController(CarControllerBase):
         self.speed_limit_offset = self.sm['longitudinalPlanSP'].speedLimitOffset
         self.v_tsc = self.sm['longitudinalPlanSP'].visionTurnSpeed
         self.m_tsc = self.sm['longitudinalPlanSP'].turnSpeed
- 
+
       if self.frame % 200 == 0:
         self.speed_limit_control_enabled = self.param_s.get_bool("EnableSlc")
         self.is_metric = self.param_s.get_bool("IsMetric")
       self.last_speed_limit_sign_tap = self.param_s.get_bool("LastSpeedLimitSignTap")
       self.v_cruise_min = FCA_V_CRUISE_MIN[self.is_metric] * (CV.KPH_TO_MPH if not self.is_metric else 1)
- 
+
     can_sends = []
- 
+
     if not self.CP.pcmCruiseSpeed:
       if not self.last_speed_limit_sign_tap_prev and self.last_speed_limit_sign_tap:
         self.sl_force_active_timer = self.frame
         self.param_s.put_bool_nonblocking("LastSpeedLimitSignTap", False)
       self.last_speed_limit_sign_tap_prev = self.last_speed_limit_sign_tap
- 
+
       sl_force_active = self.speed_limit_control_enabled and (self.frame < (self.sl_force_active_timer * DT_CTRL + 2.0))
       sl_inactive = not sl_force_active and (not self.speed_limit_control_enabled or (True if self.slc_state == 0 else False))
       sl_temp_inactive = not sl_force_active and (self.speed_limit_control_enabled and (True if self.slc_state == 1 else False))
       slc_active = not sl_inactive and not sl_temp_inactive
- 
+
       self.slc_active_stock = slc_active
- 
+
     lkas_active = CC.latActive and CS.madsEnabled
- 
+
     if self.frame % 10 == 0 and self.CP.carFingerprint not in RAM_CARS:
       can_sends.append(chryslercan.create_lkas_heartbit(self.packer, CS.lkas_disabled, CS.lkas_heartbit))
- 
+
     ram_cars = self.CP.carFingerprint in RAM_CARS
- 
+
     # Standstill brake-hold: only while the stock ACC is enabled and holding at a stop, in a
     # forward gear, with no driver override. No lead check (kept minimal to avoid messaging).
-    standstill_hold = (self.standstill_hold_enabled
-                       and not ram_cars
+    standstill_hold = (not ram_cars
                        and CS.out.cruiseState.enabled
                        and CS.out.cruiseState.standstill
                        and CS.out.gearShifter in FORWARD_GEARS
@@ -126,12 +123,12 @@ class CarController(CarControllerBase):
     else:
       self.standstill_tap_end = 0
     standstill_tapping = standstill_hold and self.frame < self.standstill_tap_end
- 
+
     das_bus = 2 if self.CP.carFingerprint in RAM_CARS else 0
     # cruise buttons
     if CS.button_counter != self.last_button_frame:
       self.last_button_frame = CS.button_counter
- 
+
       if ram_cars:
         if CS.buttonStates["cancel"]:
           can_sends.append(chryslercan.create_cruise_buttons(self.packer, CS.button_counter, das_bus, self.CP, cancel=True))
@@ -139,22 +136,22 @@ class CarController(CarControllerBase):
           can_sends.append(chryslercan.create_cruise_buttons(self.packer, CS.button_counter, das_bus, self.CP,
                                                              cruise_buttons_msg=CS.cruise_buttons,
                                                              cancel=CC.cruiseControl.cancel, resume=CC.cruiseControl.resume))
- 
+
       # ACC cancellation
       elif CC.cruiseControl.cancel:
         self.last_button_frame = self.frame
         can_sends.append(chryslercan.create_cruise_buttons(self.packer, CS.button_counter + 1, das_bus, self.CP, cancel=True))
- 
+
       # ACC resume from standstill
       elif CC.cruiseControl.resume:
         self.last_button_frame = self.frame
         can_sends.append(chryslercan.create_cruise_buttons(self.packer, CS.button_counter + 1, das_bus, self.CP, resume=True))
- 
+
       # Standstill brake-hold: momentary resume tap to beat the native ~2s release
       elif standstill_tapping:
         self.last_button_frame = self.frame
         can_sends.append(chryslercan.create_cruise_buttons(self.packer, CS.button_counter + 1, das_bus, self.CP, resume=True))
- 
+
       if not (CC.cruiseControl.cancel or CC.cruiseControl.resume or standstill_tapping) and not self.CP.pcmCruiseSpeed and CS.out.cruiseState.enabled:
         self.button_frame += 1
         button_counter_offset = [1, 1, 0, None][self.button_frame % 4]
@@ -162,23 +159,23 @@ class CarController(CarControllerBase):
           self.cruise_button = self.get_cruise_buttons(CS, CC.vCruise)
         elif button_counter_offset is not None:
           self.cruise_button = self.get_cruise_buttons(CS, CC.vCruise)
- 
+
         if self.cruise_button is not None:
           if ram_cars:
             can_sends.append(chryslercan.create_cruise_buttons(self.packer, CS.button_counter, das_bus, self.CP, buttons=self.cruise_button))
           elif button_counter_offset is not None:
             can_sends.append(chryslercan.create_cruise_buttons(self.packer, CS.button_counter + button_counter_offset, das_bus, self.CP, buttons=self.cruise_button))
- 
+
     # HUD alerts
     if self.frame % 25 == 0:
       if CS.lkas_car_model != -1:
         can_sends.append(chryslercan.create_lkas_hud(self.packer, self.CP, lkas_active, CS.madsEnabled, CC.hudControl.visualAlert,
                                                      self.hud_count, CS.lkas_car_model, CS.auto_high_beam))
         self.hud_count += 1
- 
+
     # steering
     if self.frame % self.params.STEER_STEP == 0:
- 
+
       # TODO: can we make this more sane? why is it different for all the cars?
       lkas_control_bit = self.lkas_control_bit_prev
       if self.CP.carFingerprint in RAM_DT:
@@ -196,13 +193,13 @@ class CarController(CarControllerBase):
       elif self.CP.carFingerprint in RAM_CARS:
         if CS.out.vEgo < (self.CP.minSteerSpeed - 0.5):
           lkas_control_bit = False
- 
+
       # EPS faults if LKAS re-enables too quickly
       lkas_control_bit = lkas_control_bit and (self.frame - self.last_lkas_falling_edge > 200) and not CS.out.steerFaultTemporary and not CS.out.steerFaultPermanent
- 
+
       if not lkas_control_bit and self.lkas_control_bit_prev:
         self.last_lkas_falling_edge = self.frame
- 
+
       # steer torque
       new_steer = int(round(CC.actuators.steer * self.params.STEER_MAX))
       apply_steer = apply_meas_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorqueEps, self.params)
@@ -210,17 +207,17 @@ class CarController(CarControllerBase):
         apply_steer = 0
       self.apply_steer_last = apply_steer
       self.lkas_control_bit_prev = lkas_control_bit
- 
+
       can_sends.append(chryslercan.create_lkas_command(self.packer, self.CP, int(apply_steer), lkas_control_bit))
- 
+
     self.frame += 1
- 
+
     new_actuators = CC.actuators.as_builder()
     new_actuators.steer = self.apply_steer_last / self.params.STEER_MAX
     new_actuators.steerOutputCan = self.apply_steer_last
- 
+
     return new_actuators, can_sends
- 
+
   # multikyd methods, sunnyhaibin logic
   def get_cruise_buttons_status(self, CS):
     if not CS.out.cruiseState.enabled or any(CS.buttonStates[button_state] for button_state in BUTTONS_STATES):
@@ -230,7 +227,7 @@ class CarController(CarControllerBase):
     else:
       return 1
     return 0
- 
+
   def get_target_speed(self, v_cruise_kph_prev):
     v_cruise_kph = v_cruise_kph_prev
     if self.slc_state > 1:
@@ -238,20 +235,20 @@ class CarController(CarControllerBase):
       if not self.slc_active_stock:
         v_cruise_kph = v_cruise_kph_prev
     return v_cruise_kph
- 
+
   def get_button_type(self, button_type):
     self.type_status = "type_" + str(button_type)
     self.button_picker = getattr(self, self.type_status, lambda: "default")
     return self.button_picker()
- 
+
   def reset_button(self):
     if self.button_type != 3:
       self.button_type = 0
- 
+
   def type_default(self):
     self.button_type = 0
     return None
- 
+
   def type_0(self):
     self.button_count = 0
     self.target_speed = self.init_speed
@@ -261,7 +258,7 @@ class CarController(CarControllerBase):
     elif self.target_speed < self.v_set_dis and self.v_set_dis > self.v_cruise_min:
       self.button_type = 2
     return None
- 
+
   def type_1(self):
     cruise_button = 1
     self.button_count += 1
@@ -272,7 +269,7 @@ class CarController(CarControllerBase):
       self.button_count = 0
       self.button_type = 3
     return cruise_button
- 
+
   def type_2(self):
     cruise_button = 2
     self.button_count += 1
@@ -283,14 +280,14 @@ class CarController(CarControllerBase):
       self.button_count = 0
       self.button_type = 3
     return cruise_button
- 
+
   def type_3(self):
     cruise_button = None
     self.button_count += 1
     if self.button_count > self.t_interval:
       self.button_type = 0
     return cruise_button
- 
+
   def get_curve_speed(self, target_speed_kph, v_cruise_kph_prev):
     if self.v_tsc_state != 0:
       vision_v_cruise_kph = self.v_tsc * CV.MS_TO_KPH
@@ -306,20 +303,20 @@ class CarController(CarControllerBase):
       map_v_cruise_kph = 255
     curve_speed = self.curve_speed_hysteresis(min(vision_v_cruise_kph, map_v_cruise_kph) + 2 * CV.MPH_TO_KPH)
     return min(target_speed_kph, curve_speed)
- 
+
   def get_button_control(self, CS, final_speed, v_cruise_kph_prev):
     self.init_speed = round(min(final_speed, v_cruise_kph_prev) * (CV.KPH_TO_MPH if not self.is_metric else 1))
     self.v_set_dis = round(CS.out.cruiseState.speed * (CV.MS_TO_MPH if not self.is_metric else CV.MS_TO_KPH))
     cruise_button = self.get_button_type(self.button_type)
     return cruise_button
- 
+
   def curve_speed_hysteresis(self, cur_speed: float, hyst=(0.75 * CV.MPH_TO_KPH)):
     if cur_speed > self.steady_speed:
       self.steady_speed = cur_speed
     elif cur_speed < self.steady_speed - hyst:
       self.steady_speed = cur_speed
     return self.steady_speed
- 
+
   def get_cruise_buttons(self, CS, v_cruise_kph_prev):
     cruise_button = None
     if not self.get_cruise_buttons_status(CS):
@@ -334,7 +331,6 @@ class CarController(CarControllerBase):
         self.final_speed_kph = self.get_curve_speed(target_speed_kph, v_cruise_kph_prev)
       else:
         self.final_speed_kph = target_speed_kph
- 
+
       cruise_button = self.get_button_control(CS, self.final_speed_kph, v_cruise_kph_prev)  # MPH/KPH based button presses
     return cruise_button
- 
